37466b2063a4e367ab700a9e626d07587b510089
Daemian Mack <dmack@navisite.com>
2010-06-22 20:23:43 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,58 @@
+What's this thing?
+=================
+
+This script...
+
+- harvests user-generated pictures out of camphone emails,
+- posts them to an upload utility, and 
+- cross-posts the resulting URL to a webforum, masquerading as the authenticated user, and finally,
+- allows users to register with the service so they can do the above.
+
+It'd be trivial to replace bulletpoint 2 with 'sends them off to a mailing list/flickr/Facebook' or replace bulletpoint 3 with 'posts the URL to my blog/tumblog/digital pictureframe/Times Square bulletin board', etc.
+
+Mail comes in via fetchmail/procmail or whatever your favorite mail-handling system might be. I've included the fetchmail config for completeness.
+
+
+This script will accept two flavors of email:
+--------------------------------------------
+
+1. **Registration** email. If this script finds a subject starting with the string 'reg', it will create an entry in a DB table ('camphone_user') using the first word of the email body as the username and the second word as the password. It will also use the sender address as an identifier. (Depending on carrier, this may be the phone number, or it may be the human-friendly From: field of the sending email client. In either case, it serves to distinguish the sending device.)
+
+    After registering a user, the script will reply to their email address with a confirmation.
+
+       Subj: register
+       Body: [USERNAME] [PASSWORD]
+
+
+2. **Camphone** email. If this script does not find a subject starting with 'reg', and instead finds an image attached, it will parse the email body for a password, the identifying string, and optionally a caption bookended in double Qs.
+
+       Subj: N/A
+       Body: [PASSWORD] qq[SOME TEXT]qq
+
+That is, the first word in the body of the email must be the password stored for the user via their registration email. Optionally, they can add a text caption to the photo that will be added to the forum post. I've chosen 'qq' as a sentinel since it's a rare sequence. This way we clearly delineate password from caption and other text, which carriers will tack on without regard for our sanity. Users whose emails must contain disclaimers and such can also use the optional 'qq' sentinel at the end of their caption, to bookend it.
+
+
+
+### Also
+
+I'm including two sample mailfiles, one for each flavor of email. You can use these to test your own setup, format changes, etc.
+
+Note that you'll need Github's version of MMS2R...
+
+    sudo gem sources -a http://gems.github.com
+    sudo gem install monde-mms2r
+
+as the version available via the vanilla gem sources causes TMail to emit errors regarding 'return_path' or 'with_indifferent_access' being undefined methods.
+
+
+MySQL schema of camphone_users...
+
+    CREATE TABLE `camphone_users` (
+      `username` varchar(25) NOT NULL,
+      `identifier` varchar(255) NOT NULL,
+      `password` varchar(50) DEFAULT NULL,
+      UNIQUE KEY `username` (`username`,`identifier`)
+    )
+
+
+This is all possible thanks to the wondrous MMS2R gem made available by Mike Mondragon, which must represent hours of patience sorting through the aberrant things providers do with, to, and alongside the MMS messages you send through their systems. Startling but true footnote follows: A canadian colleague of mine is on Sasktel. Every single one of his MMSs come through with a little surprise gift from his carrier: the entire contents of an XHTML Basic 1.0 DTD as an inline attachment to his message. It's 1500 lines long and takes up 63k. (I can't figure out whether this would be more preposterous if he were paying by the kilobyte, or on a free data plan.)
---
+++ engine.rb
@@ -0,0 +1,176 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'mms2r'
+require 'tmail'
+require 'fileutils'
+require 'ostruct'
+require 'mini_magick'
+require 'logger'
+require 'mysql'
+require 'net/smtp'
+require 'mechanize'
+
+
+@opts = OpenStruct.new(
+                       :dbhost         => "localhost",
+                       :db             => "database",
+                       :dbuser         => "camphone",
+                       :dbpass         => "camphone_password",
+                       :forum_id       => 100,
+                       :thread_id      => 2000,
+                       :upload_to      => "http://upload.example.com/",
+                       :cookie         => "/Users/daemian/bin/camphone/cookie",
+                       :uploaded_xpath => "//div[@id='success']",
+                       :smtphost       => "localhost",
+                       :logfile        => "camphone_mails.log",
+                       :reply_bcc      => "user@example.com",
+                       :reply_from     => "camphone_bot@example.com"
+                       )
+
+@dbh = Mysql.real_connect(@opts.dbhost, @opts.dbuser, @opts.dbpass, @opts.db)
+
+
+
+def log(message)
+  logfile = File.open(@opts.logfile, "a") do |f|
+    f << Time.now.strftime('%a %b %d %I:%M%p') + ": " + message + "\n"
+  end
+end
+
+def handle_registrations(mms, email)
+  username = IO.readlines(mms.default_text.path)
+  username = username.to_s.gsub(/<\/?[^>]*>/, "").split[0].to_s  # Get first word: username.
+  log "Username: #{username}"
+  password = IO.readlines(mms.default_text.path)
+  password = password.to_s.gsub(/<\/?[^>]*>/, "").split[1].to_s  # Get second word: password.
+  log "Password: #{password}"
+  q = @dbh.prepare("insert into camphone_users (username, identifier, password) values (?, ?, ?)")
+  if q.execute(username.downcase, mms.number, password)
+    send_mail(email, "This device is now registered.")
+  end
+end
+
+def find_username(mms)
+  passwords = []
+  if mms.default_text
+    passwords << IO.readlines(mms.default_text.path).join.split.slice!(0..0).to_s.split("Attachment")[0] # Get first word and password.
+    mms.media['text/plain'].each do |text|
+      # Some carriers attach the user message as a file. Loop through all files, grab the first word, and we'll try each.
+      passwords << IO.readlines(text).join.split.slice!(0..0).to_s.split("Attachment")[0].gsub(/<\/?[^>]*>/, "")
+    end
+  end
+  q = @dbh.prepare("select username from camphone_users where identifier = ? and password = ? limit 1")
+  username = nil
+  passwords.each do |candidate|
+    log "Firing db lookup for password: #{candidate}"
+    break if username = q.execute(mms.number, candidate).fetch
+  end
+  if username.nil?
+    log "Username not found! Identifier: #{mms.number}, password: #{passwords.join(',')}." and exit end
+  username.to_s.downcase
+end
+
+def send_mail(email, subject)
+  reply = email.create_reply
+  reply.subject = subject
+  reply.bcc = @opts.reply_bcc
+  reply.from = @opts.reply_from
+  reply.mime_version = '1.0'
+  reply.set_content_type 'text', 'plain'
+  msg = reply.encoded
+
+  smtp = Net::SMTP.new(@opts.smtphost, 25)
+  smtp.start {
+    smtp.send_mail msg, reply.from, reply.destinations
+  }
+  log "Sent mail to address #{reply.to} with subject: #{subject}."
+end
+
+def find_biggest_image(mms)
+  biggest_image = mms.default_media.path rescue nil
+  if biggest_image.nil?
+    images = []
+    mms.media['application/octet-stream'].collect {|x| images << x if x =~ /jpg$|gif$/}  # Search any known hashes for jpg|gif strings.
+    sorted_images = images.uniq.sorted_by {|x| File.size(x)} # Sort them by size.
+    sorted_images.reverse!  # Put the biggest in front.
+    biggest_image = sorted_images[0]  # Our biggest image *should* be the user's. 
+    log "Had to brute-force the media bits to find the biggest image."
+  end
+  biggest_image
+end
+
+def find_caption(mms)
+  text = ""
+  if mms.default_text
+    rawtext = IO.readlines(mms.default_text.path).join.split.slice!(1..-1).join(" ")  # Remove first word -- their password.
+  end
+  text = rawtext.slice(2..-1) if rawtext =~ /^qq/  # If 'qq...' exists, we've got a caption.
+  text = text.split("qq")[0] unless text.nil?  # Let's allow qq delimiter on both sides of the caption.
+  log "Finished text search; text: #{text}" if text
+  text
+end  
+
+
+def upload_image(username, image, text)
+  agent = Mechanize.new
+  agent.cookie_jar.load(@opts.cookie)
+  page = agent.get(@opts.upload_to)
+  form = page.forms[0]
+  form.multiple = username.downcase
+  form.file_uploads.first.file_name = image
+  page = form.submit
+  uploaded = page.search(@opts.uploaded_xpath).inner_html
+  if !uploaded.empty?
+    log "Got link from uploader: #{uploaded}."
+  else
+    log "Upload failed."
+    exit
+  end
+  message = "#{uploaded}"
+  message << "<br/><br/>" + text unless text.nil?
+  message
+end
+
+def do_forum_post(username, message)
+  q = @dbh.prepare("insert into posts (fid, tid, author, message, dateline) values (?,?,?,?,?)")
+  if q.execute(@opts.forum_id, @opts.thread_id, username, message, Time.now.to_i.to_s)
+    log "Executed post insert."
+  else
+    log "Error inserting post. Username: #{username}, message: #{message}"
+  end
+end
+
+
+# Accept fetchmail's output on STDIN.
+incoming = ""
+while line = $stdin.gets do
+  incoming << line
+end
+
+email = TMail::Mail.parse(incoming)
+log "----------------"
+log "Received email from #{email.from}."
+mms = MMS2R::Media.new(email)
+
+
+# Main. Are we dealing with a registration, or a picture mail?
+if email.subject.strip =~ /^reg/i
+  log "Email had a subject of /reg*/, so we're dealing with a registration."
+  handle_registrations(mms, email)
+else
+  log "Looks like a picture mail."
+  begin
+    username = find_username(mms)
+    biggest_image = find_biggest_image(mms)
+    text = find_caption(mms)
+    message = upload_image(username, biggest_image, text)
+    
+    mms.purge  # Delete tempfiles.
+
+    do_forum_post(username, message)
+  rescue Exception => e
+    log e.message
+    log e.backtrace.inspect
+  end
+end
